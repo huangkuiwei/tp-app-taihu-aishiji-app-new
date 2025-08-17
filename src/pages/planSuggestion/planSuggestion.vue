ba83f0d1e24d40fc0eb417c95806a7e9e48e0186
huangkuiwei <1017969390@qq.com>
2025-08-17 15:29:18 +0800

    提交

--- src/pages/planSuggestion/planSuggestion.vue
+++ src/pages/planSuggestion/planSuggestion.vue
@@ -1,136 +1,104 @@
 <template>
   <view class="plan-suggestion-page">
     <view class="page-title">
-      <text>方案建议</text>
+      <text></text>
 
-      <view class="back" @click="$toSwitch('/pages/index/index')">
-        <uni-icons class="back" color="#1A1A1A" type="left" size="22"></uni-icons>
-      </view>
+      <!--<view class="back" @click="$toSwitch('/pages/index/index')">-->
+      <!--  <uni-icons class="back" color="#1A1A1A" type="left" size="22"></uni-icons>-->
+      <!--</view>-->
     </view>
 
-    <view class="banner"> </view>
+    <view class="banner"></view>
 
-    <view class="suggest-box-wrap">
-      <view class="suggest-box">
-        <view class="title">您的专属计划已生成！</view>
+    <view class="plan-box">
+      <view class="plan-title">你的专属计划已生成</view>
 
-        <view class="head">
-          <image
-            class="head-img"
-            mode="aspectFill"
-            :src="
-              userInfo.avatar_url || 'https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/my/default_head.png'
-            "
-          />
+      <view class="plan-time">
+        计划将持续 <text>{{ Math.ceil(planDays / 7) }}</text> 周，每周平均减重
+        <text>{{ planSuggestion.weekly_loss_weight }}</text
+        >kg
+      </view>
 
-          <view class="suggest-list">
-            <view class="suggest-item suggest-item1">
-              <image
-                mode="widthFix"
-                src="https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/planSuggestion/dialog01.png"
-              />
+      <view class="card-box">
+        <view class="card-item1">
+          <view class="card-title">
+            <text>您的身体评测报告</text>
+          </view>
 
-              <text>BMI {{ planSuggestion.plan_initial_bmi }}</text>
+          <view class="card-title2">
+            <text>根据您的身高体重测算，推荐BMI范围： </text>
+          </view>
+
+          <view class="chart-box">
+            <view class="chart">
+              <l-echart ref="chartRef1" @finished="init1" />
             </view>
 
-            <view class="suggest-item suggest-item2">
-              <image
-                mode="widthFix"
-                src="https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/planSuggestion/dialog02.png"
-              />
+            <view class="chart-data">
+              <text>{{ planSuggestion.body_score }}</text>
+              <text>身体评分</text>
+            </view>
+          </view>
 
-              <text>身高 {{ planSuggestion.height }}cm</text>
+          <view class="bmi">
+            初始BMI值为：
+            <text>{{ planSuggestion.plan_initial_bmi }}</text>
+          </view>
+
+          <view class="plan-detail">
+            <view class="plan-item">
+              <view class="title" style="background: linear-gradient(90deg, #5664e5 0%, #ffffff 100%)">饮食策略</view>
+
+              <view class="content">
+                <view class="content-item">
+                  <text>营养结构</text>
+                  <text>高蛋白均衡</text>
+                </view>
+
+                <view class="content-item">
+                  <text>碳水</text>
+                  <text>40%</text>
+                </view>
+
+                <view class="content-item">
+                  <text>蛋白</text>
+                  <text>30%</text>
+                </view>
+
+                <view class="content-item">
+                  <text>脂肪</text>
+                  <text>30%</text>
+                </view>
+
+                <view class="content-item">
+                  <text>控糖方案</text>
+                  <text>添加糖 ≤20g/天</text>
+                </view>
+              </view>
             </view>
 
-            <view class="suggest-item suggest-item3">
-              <image
-                mode="widthFix"
-                src="https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/planSuggestion/dialog03.png"
-              />
+            <view class="plan-item">
+              <view class="title" style="background: linear-gradient(90deg, #fd6896 0%, #ffffff 100%)">运动策略</view>
 
-              <text>基础代谢 {{ planSuggestion.plan_initial_bmr }}</text>
-            </view>
+              <view class="content1">
+                <view class="content-item">
+                  <text>日均消耗目标</text>
+                  <text>500千卡</text>
+                </view>
 
-            <view class="suggest-item suggest-item4">
-              <image
-                mode="widthFix"
-                src="https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/planSuggestion/dialog04.png"
-              />
+                <view class="content-item">
+                  <text>有氧运动</text>
+                  <text>2次/周·低强度</text>
+                </view>
 
-              <text>体重 {{ planSuggestion.current_weight }}公斤</text>
+                <view class="content-item">
+                  <text>无氧运动</text>
+                  <text>1次/周·高强度</text>
+                </view>
+              </view>
             </view>
           </view>
         </view>
-
-        <view class="info">
-          <view class="bmi">初始BMI值为：{{ planSuggestion.plan_initial_bmi }}</view>
-
-          <view class="info1">
-            <view>
-              根据您的身高体重测算，推荐BMI范围：
-              <text>18.5-24.9</text>
-            </view>
-
-            <view>
-              您的标准体重区间为
-              <text>{{ weightData.min }}公斤——{{ weightData.max }}公斤</text>
-            </view>
-
-            <view>
-              您大概需要
-              <text>{{ Math.ceil(planDays / 7) }}周（{{ planDays }}天）</text>
-              时间达成目标
-            </view>
-          </view>
-        </view>
-
-        <view class="suggest-list2">
-          <view class="suggest-item">
-            <view class="suggest-title">建议每日热量缺口</view>
-            <view class="unit">
-              <text>200</text>
-              千卡
-            </view>
-          </view>
-
-          <view class="suggest-item">
-            <view class="suggest-title">建议运动频率</view>
-            <view class="unit">
-              <text>2-3次</text>
-              /每周
-            </view>
-          </view>
-
-          <view class="suggest-item">
-            <view class="suggest-title">建议饮食节奏</view>
-            <view class="unit">
-              <text>12+10</text>
-              轻断食
-            </view>
-          </view>
-
-          <view class="suggest-item">
-            <view class="suggest-title">建议饮食习惯</view>
-            <view class="unit">
-              <text>少油少盐</text>
-            </view>
-          </view>
-        </view>
-
-        <view class="countdown">
-          <view class="countdown-title">计划剩余时间</view>
-
-          <view class="time">
-            <text class="number" v-for="(item, index) of countdownDays" :key="index">{{ item }}</text>
-            <text class="unit">天</text>
-            <text class="number" v-for="(item, index) of countdownHours" :key="index">{{ item }}</text>
-            <text class="unit">小时</text>
-            <text class="number" v-for="(item, index) of countdownMinutes" :key="index">{{ item }}</text>
-            <text class="unit">分钟</text>
-          </view>
-        </view>
-
-        <view class="submit-btn" @click="submit">开启好身材计划</view>
       </view>
     </view>
   </view>
@@ -138,6 +106,9 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex';
+import * as echarts from '@/uni_modules/lime-echart/static/echarts.min';
+
+let chart1 = null;
 
 export default {
   name: 'planSuggestion',
@@ -148,6 +119,49 @@ export default {
       countdownDays: '',
       countdownHours: '',
       countdownMinutes: '',
+      option1: {
+        series: [
+          {
+            type: 'gauge',
+            startAngle: 180,
+            endAngle: 0,
+            min: 0,
+            max: 100,
+            center: ['50%', '100%'], // 水平居中，垂直方向下移一点（默认是 '50%', '50%'）
+            radius: '75%',
+            axisLine: {
+              lineStyle: {
+                width: 15,
+              },
+            },
+            pointer: {
+              show: false,
+            },
+            axisTick: {
+              distance: -17,
+              length: 19,
+              lineStyle: {
+                color: '#fff',
+                width: 4,
+              },
+            },
+            splitLine: {
+              show: false,
+            },
+            axisLabel: {
+              show: false,
+            },
+            detail: {
+              show: false,
+            },
+            data: [
+              {
+                value: 0,
+              },
+            ],
+          },
+        ],
+      },
     };
   },
 
@@ -175,14 +189,25 @@ export default {
   },
 
   onLoad() {
-    this.planSuggestion = uni.getStorageSync('planSuggestion');
-    console.log('this.planSuggestion', this.planSuggestion);
+    this.planSuggestion = uni.getStorageSync('planSuggestion') || {};
 
-    this.countDown();
+    this.option1.series[0].data[0].value = this.planSuggestion.body_score;
 
-    setInterval(() => {
-      this.countDown();
-    }, 2000);
+    let color = [];
+
+    for (let i = 0; i < 100; i++) {
+      if (i <= this.planSuggestion.body_score) {
+        color.push([i / 100, '#5664E5']);
+      } else {
+        color.push([i / 100, '#F2F5FF']);
+      }
+    }
+
+    this.option1.series[0].axisLine.lineStyle.color = color;
+
+    setTimeout(() => {
+      chart1.setOption(this.option1);
+    }, 500);
   },
 
   onShareAppMessage() {
@@ -194,19 +219,9 @@ export default {
   },
 
   methods: {
-    countDown() {
-      let end_date = this.planSuggestion.end_date && this.planSuggestion.end_date.replace(/-/g, '/');
-      let diff = new Date(end_date).getTime() - new Date().getTime();
-
-      const minutes = Math.floor(diff / 1000 / 60);
-      const hours = Math.floor(minutes / 60);
-      const days = Math.floor(hours / 24);
-      const remainingHours = hours % 24;
-      const remainingMinutes = minutes % 60;
-
-      this.countdownDays = days.toString();
-      this.countdownHours = remainingHours.toString();
-      this.countdownMinutes = remainingMinutes.toString();
+    async init1() {
+      chart1 = await this.$refs.chartRef1.init(echarts);
+      chart1.setOption(this.option1);
     },
 
     submit() {
@@ -223,235 +238,219 @@ export default {
 <style lang="scss">
 page {
   height: 100%;
+  background: #f6f7fb url('https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app2/weightManagementPlan/bg.png')
+    left top/100% auto no-repeat;
 }
 </style>
 
 <style scoped lang="scss">
 .plan-suggestion-page {
-  height: 100%;
-  overflow: auto;
-  padding-bottom: 240rpx;
-  background: linear-gradient(180deg, rgba(204, 255, 238, 0.9) 0%, rgba(251, 255, 255, 0.93) 27%, #f6f7fb 100%);
-
   .page-title {
+    // background: #ffffff;
   }
 
   .banner {
-    padding: calc(var(--page-title-height)) 0 0;
+    padding: calc(var(--page-title-height) + 9rpx) 0 0;
+    // background: #ffffff;
   }
 
-  .suggest-box-wrap {
-    padding: 60rpx 30rpx 0;
+  .plan-box {
+    padding: 43rpx 30rpx 100rpx;
 
-    .suggest-box {
-      background: #ffffff;
-      border-radius: 25rpx;
-      padding: 38rpx 30rpx;
+    .plan-title {
+      font-weight: bold;
+      font-size: 40rpx;
+      color: #111111;
+      margin-bottom: 28rpx;
+    }
 
-      .title {
-        font-weight: 500;
-        font-size: 32rpx;
-        color: #333333;
-        text-align: center;
-        margin-bottom: 115rpx;
+    .plan-time {
+      font-size: 28rpx;
+      color: #999999;
+      margin-bottom: 31rpx;
+
+      text {
+        color: #5664e5;
+        font-size: 40rpx;
+        font-weight: bold;
+        padding: 0 8rpx;
       }
+    }
 
-      .head {
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-bottom: 60rpx;
+    .card-box {
+      .card-item1 {
+        background: #ffffff;
+        border-radius: 20rpx;
+        margin-bottom: 22rpx;
 
-        .head-img {
-          width: 148rpx;
-          height: 148rpx;
-          border-radius: 50%;
-        }
-
-        .suggest-list {
-          position: absolute;
-          left: 0;
-          right: 0;
-          top: 0;
-          bottom: 0;
-
-          .suggest-item {
-            text {
-              position: absolute;
-              left: 0;
-              right: 0;
-              top: 0;
-              bottom: 20rpx;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              font-size: 24rpx;
-              color: #ffffff;
-            }
-          }
-
-          .suggest-item1 {
-            position: absolute;
-            left: 114rpx;
-            top: -60rpx;
-
-            image {
-              width: 144rpx;
-            }
-          }
-
-          .suggest-item2 {
-            position: absolute;
-            right: 114rpx;
-            top: -60rpx;
-
-            image {
-              width: 154rpx;
-            }
-          }
-
-          .suggest-item3 {
-            position: absolute;
-            left: 47rpx;
-            bottom: 12rpx;
-
-            image {
-              width: 190rpx;
-            }
-          }
-
-          .suggest-item4 {
-            position: absolute;
-            right: 47rpx;
-            bottom: 12rpx;
-
-            image {
-              width: 184rpx;
-            }
-          }
-        }
-      }
-
-      .info {
-        .bmi {
-          font-weight: 500;
-          font-size: 28rpx;
-          color: #1a1a1a;
-          margin-bottom: 22rpx;
-        }
-
-        .info1 {
-          font-size: 22rpx;
-          color: #999999;
-
-          view {
-            margin-bottom: 20rpx;
-
-            text {
-              color: #ffa537;
-            }
-          }
-        }
-      }
-
-      .suggest-list2 {
-        margin-bottom: 56rpx;
-
-        .suggest-item {
-          background: #eff5fe;
-          border-radius: 25rpx;
-          padding: 30rpx;
+        .card-title {
+          padding: 36rpx 19rpx 0;
+          position: relative;
+          z-index: 9;
           display: flex;
           align-items: center;
           justify-content: space-between;
+          margin-bottom: 20rpx;
 
-          &:not(:last-child) {
-            margin-bottom: 12rpx;
-          }
+          text {
+            &:nth-child(1) {
+              font-weight: 500;
+              font-size: 30rpx;
+              color: #1a1a1a;
+            }
 
-          &:nth-child(1) {
-            background: #eff5fe;
-          }
-
-          &:nth-child(2) {
-            background: #fffae1;
-          }
-
-          &:nth-child(3) {
-            background: #fef4f3;
-          }
-
-          &:nth-child(4) {
-            background: #e4fff7;
-          }
-
-          .suggest-title {
-            font-size: 28rpx;
-            color: #555555;
-          }
-
-          .unit {
-            font-weight: 500;
-            font-size: 24rpx;
-            color: #555555;
-
-            text {
-              font-size: 36rpx;
-              color: #ffa537;
+            &:nth-child(2) {
+              font-size: 26rpx;
+              color: #5168e6;
             }
           }
         }
-      }
 
-      .countdown {
-        .countdown-title {
+        .card-title2 {
+          padding: 0 19rpx 0;
           font-size: 22rpx;
           color: #999999;
-          text-align: center;
-          margin-bottom: 36rpx;
+          margin-bottom: 55rpx;
         }
 
-        .time {
+        .chart-box {
+          padding: 0 19rpx 0;
+          height: 280rpx;
+          position: relative;
+
+          .chart {
+            position: relative;
+            top: -350rpx;
+          }
+
+          .chart-data {
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 50rpx;
+            z-index: 9;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 14rpx;
+
+            text {
+              &:nth-child(1) {
+                font-weight: 500;
+                font-size: 76rpx;
+                color: #5664e5;
+              }
+
+              &:nth-child(2) {
+                font-size: 22rpx;
+                color: #999999;
+              }
+            }
+          }
+        }
+
+        .bmi {
+          padding: 0 19rpx 0;
+          width: 580rpx;
+          height: 60rpx;
+          margin: 0 auto 66rpx;
+          background: #f2f5ff;
+          border-radius: 30rpx;
           display: flex;
           align-items: center;
           justify-content: center;
+          font-size: 28rpx;
+          color: #333333;
 
-          .number {
-            width: 55rpx;
-            height: 70rpx;
-            background: #0abf92;
-            border-radius: 10rpx;
-            font-weight: bold;
-            font-size: 45rpx;
-            color: #ffffff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin: 0 5rpx;
-          }
-
-          .unit {
-            font-size: 22rpx;
-            color: #999999;
-            margin: 0 5rpx;
+          text {
+            color: #5664e5;
           }
         }
-      }
 
-      .submit-btn {
-        position: fixed;
-        bottom: 80rpx;
-        width: 592rpx;
-        height: 103rpx;
-        background: #0abf92;
-        border-radius: 52rpx;
-        font-weight: 500;
-        font-size: 32rpx;
-        color: #ffffff;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+        .plan-detail {
+          .title {
+            height: 60rpx;
+            display: flex;
+            align-items: center;
+            padding: 0 18rpx;
+            font-weight: 500;
+            font-size: 30rpx;
+            color: #ffffff;
+            margin-bottom: 29rpx;
+          }
+
+          .content {
+            display: flex;
+            flex-wrap: wrap;
+            padding-left: 59rpx;
+
+            .content-item {
+              white-space: nowrap;
+              display: flex;
+              flex-direction: column;
+              gap: 20rpx;
+              margin-bottom: 40rpx;
+
+              &:nth-child(4n-3) {
+                width: 40%;
+              }
+
+              &:nth-child(2) {
+                width: 20%;
+              }
+
+              &:nth-child(3) {
+                width: 20%;
+              }
+
+              &:nth-child(4) {
+                width: 20%;
+              }
+
+              text {
+                &:nth-child(1) {
+                  font-size: 24rpx;
+                  color: #999999;
+                }
+
+                &:nth-child(2) {
+                  font-weight: 500;
+                  font-size: 26rpx;
+                  color: #111111;
+                }
+              }
+            }
+          }
+
+          .content1 {
+            display: flex;
+            flex-wrap: wrap;
+            padding-left: 20rpx;
+
+            .content-item {
+              white-space: nowrap;
+              display: flex;
+              flex-direction: column;
+              gap: 20rpx;
+              margin-bottom: 40rpx;
+              width: 33%;
+
+              text {
+                &:nth-child(1) {
+                  font-size: 24rpx;
+                  color: #999999;
+                }
+
+                &:nth-child(2) {
+                  font-weight: 500;
+                  font-size: 26rpx;
+                  color: #111111;
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
