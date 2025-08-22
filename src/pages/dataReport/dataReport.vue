28162d501005f2fb04b8fb0983db6d6b010b4c3f
huangkuiwei <1017969390@qq.com>
2025-08-22 17:08:18 +0800

    提交

--- src/pages/dataReport/dataReport.vue
+++ src/pages/dataReport/dataReport.vue
@@ -317,7 +317,7 @@ export default {
   onShareAppMessage() {
     return {
       title: 'AI饮食记录小程序',
-      imageUrl: 'https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app/share-img.jpg',
+      imageUrl: 'https://hnenjoy.oss-cn-shanghai.aliyuncs.com/food-diary-app2/share-img.jpg',
       path: '/pages/index/index',
     };
   },
